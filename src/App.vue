b242e7b91430280e21decb4b646df05245955b6a
jrbanda1 <jrbanda1@stu.southtexascollege.edu>
2023-09-07 02:14:37 -0500

    added styling to the main

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,36 @@
 <script setup></script>
 
 <template>
-  <h1>Hello There</h1>
+  <main>
+    <h1 class="title"> :) Vue Grocery List :)</h1>
+    <form class="newGroceryFrom">
+        <input id="newGrocery" 
+        autocomplete="off"
+        type="text" 
+        placeholder="Add an item to your list">
+        <button id="addBtn" type ="submit">Add</button>
+    </form>
+  </main>
+  
 </template>
 
-<style scoped></style>
+<style lang="postcss" scoped>
+  main{
+    @apply mt-8 flex flex-col justify-center items-center;
+    .title{
+      @apply m-2 text-6xl font-light tracking-wider text-accent;
+    }
+  }
+  form{
+    @apply mt-8 flex focus-within:ring-8 focus-within:ring-accent focus-within:rounded-lg;
+    input{
+      @apply bg-white text-comment p-2 w-80 text-xl rounded-l-md outline-none;
+    }
+    button{
+      @apply bg-accent text-background p-2 text-2xl font-bold rounded-r-md;
+      &:hover{
+        @apply bg-purplish;
+      }
+    }
+  }
+</style>
